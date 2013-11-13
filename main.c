b4c2924ab8314e9fd1f5e41a58b07acbb9eecc57
chadrrobbins <chad.r.robbins@gmail.com>
2013-11-13 13:22:38 -0800

    Begin PA7

---
+++ main.c
@@ -0,0 +1,8 @@
+#include "math.h"
+
+int main (void)
+{
+
+
+	return 0;
+}
---
+++ math.h
@@ -0,0 +1,10 @@
+#ifndef MATH_H
+#define MATH_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+
+#endif
